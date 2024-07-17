b3a4f4d345ae0041d0bc20eb5255716bb737262f
JorgeVera <JorgeVeraZambrano@outlook.com>
2024-07-17 00:58:40 -0500

    XD

---
+++ PuntoDeVentaAPI/Controllers/PartidoController/PartidoController.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using Data.Interfaces.ApiCandidatosInterfaces;
+using Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLog;
+using PuntoDeVentaAPI.Services;
+using PuntoDeVentaData.Dto.UtilitiesDTO;
+
+namespace PuntoDeVentaAPI.Controllers.PartidoController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class PartidoController : ControllerBase
+    {
+
+        private readonly PartidoInterface _partidoInterface;
+        private readonly ApplicationDbContext _context;
+        private readonly IServiceProvider _service;
+        private readonly IMapper _mapper;
+        private readonly IConfiguration _configuration;
+        private readonly ApplicationUserManager _userManager;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private static Logger _log = LogManager.GetLogger("PartidoController");
+        MessageInfoDTO infoDTO = new MessageInfoDTO();
+        public readonly string _usuario;
+        private readonly string _ip;
+        private readonly string _nombreController;
+
+        public PartidoController(PartidoInterface partidoInterface, ApplicationDbContext applicationDbContext, IHttpContextAccessor httpContextAccessor, ApplicationUserManager userManager, IServiceProvider service, IMapper mapper, IConfiguration configuration)
+        {
+            _partidoInterface = partidoInterface;
+            this._context = applicationDbContext;
+            _service = service;
+            _mapper = mapper;
+            _configuration = configuration;
+            _nombreController = "PartidoController";
+            _httpContextAccessor = httpContextAccessor;
+            _ip = _httpContextAccessor?.HttpContext?.Connection.RemoteIpAddress?.ToString() ?? string.Empty;
+            _usuario = Task.Run(async () =>
+            (await userManager.FindByNameAsync(httpContextAccessor.HttpContext?.User.Claims.FirstOrDefault(c =>
+            c.Type.Contains("email", StringComparison.CurrentCultureIgnoreCase))?.Value ?? ""))?.UserName ?? "Desconocido").Result;
+        }
+
+
+    }
+}
---
+++ PuntoDeVentaAPI/Controllers/PocisionController/PocisionController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PuntoDeVentaAPI.Controllers.PocisionController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PocisionController : ControllerBase
+    {
+    }
+}
